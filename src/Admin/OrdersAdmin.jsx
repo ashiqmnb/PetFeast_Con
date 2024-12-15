d0da4e3ae527342dc316ac6f6d6fdd258a594246
ashiqmnb <ashiqamohammed@gmail.com>
2024-12-15 13:23:13 +0530

    feat(OrderAdmin): add update order status functionality

--- src/Admin/OrdersAdmin.jsx
+++ src/Admin/OrdersAdmin.jsx
@@ -2,12 +2,23 @@ import React, { useEffect, useState } from 'react';
 import access from '../assets/access.png';
 import SideBar from './SideBar';
 import axios from 'axios';
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdPending } from "react-icons/md";
+import { MdCancel } from "react-icons/md";
+import { MdLocalShipping } from "react-icons/md";
+import { useNavigate } from 'react-router-dom';
+
+
 
 const OrdersAdmin = () => {
 
   const role = localStorage.getItem('role');
+  const navigate = useNavigate();
 
   const [ orderDetails, setOrderDetails ] = useState([]);
+  const [ updateStatus, setUpdateStatus ] = useState(false);
+  const [ currentOrder, setCurrentOrder ] = useState({});
+  const [ orderStatus, setOrderStatus] = useState("");
   
   useEffect(()=>{
     axios.get("https://localhost:7109/api/Order/GetOrderDetailsAdmin",
@@ -17,7 +28,7 @@ const OrdersAdmin = () => {
         }
       })
       .then((res)=>{
-        console.log("order res",res.data.data)
+        // console.log("order res",res.data.data)
         setOrderDetails(res.data.data)
       })
       .catch((err)=>{
@@ -25,7 +36,47 @@ const OrdersAdmin = () => {
       })
   },[])
 
+  const showUpdateModal = (id)=>{
+    setUpdateStatus(true);
+    const selectedOrder = orderDetails.find(o => o.id == id);
+    setCurrentOrder(selectedOrder);
+    setOrderStatus(selectedOrder.orderstatus);
+    console.log("orderstatus", selectedOrder.orderstatus);
+  }
 
+  const handleUpdateStatus = (id, status)=>{
+    axios.patch(`https://localhost:7109/api/Order/UpdateOrderStatus?orderId=${id}&newStatus=${status}`,
+      {},
+      {
+        headers:{
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      })
+      .then((res)=>{
+        // console.log("order update res", res);
+
+        // Reset the all order details
+        axios.get("https://localhost:7109/api/Order/GetOrderDetailsAdmin",
+          {
+            headers:{
+              Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+          })
+          .then((res)=>{
+            // console.log("order res",res.data.data)
+            setOrderDetails(res.data.data)
+          })
+          .catch((err)=>{
+            console.log("order err", err)
+          })
+
+        
+        setUpdateStatus(false)
+      })
+      .catch((err)=>{
+        console.log("order update err", err)
+      })
+  }
 
 
 
@@ -42,12 +93,72 @@ const OrdersAdmin = () => {
   }
 
   return (
-    <div>
-      <div>
+    <div className='relative'>
+
+      <div className={`${
+            updateStatus ? 'filter blur-md' : ''
+        }`}>
         <SideBar/>
       </div>
 
-      <div className=" my-20 mt-12 md:ms-64 ms-24">
+      {/* Update Order Status Modal */}
+      {updateStatus ? (
+        <div className='absolute text-lg bg-blue-200 h-[350px] p-5 w-[600px] top-40 left-1/3 z-10 rounded-lg shadow-lg space-y-1'>
+          <p style={{background:'#052560'}} className='text-white p-2 rounded-lg shadow-m mb-2'>
+            <span>Customer Name : </span>
+            <span className='font-serif'>{currentOrder.customerName}</span>
+          </p>
+          <p>
+            <span className='font-semibold'>Email : </span>
+            <span>{currentOrder.customerEmail}</span>
+          </p>
+          <p>
+            <span className='font-semibold'>Order Date : </span>
+            <span>{currentOrder.orderDate.substring(0, 10)}</span>
+          </p>
+          <p>
+            <span className='font-semibold'>Order ID : </span>
+            <span>{currentOrder.orderId}</span>
+          </p>
+          <p>
+            <span className='font-semibold'>Total Price : ₹ </span>
+            <span>{currentOrder.totalPrice}</span>
+          </p>
+          <p>
+            <span className='font-semibold'>Transactiom ID : </span>
+            <span>{currentOrder.transactionId}</span>
+          </p>
+
+          <h1 className='pt-3 font-semibold'>Select Order Status :</h1>
+          <div className='flex justify-between gap-2'>
+            <select
+              // onChange={handleDropdownChange}
+              className="w-72 border border-gray-300 rounded-lg"
+              value={orderStatus}
+              onChange={(e) => setOrderStatus(e.target.value)}
+            >
+              <option value="Pending" >Pending</option>
+              <option value="Shipped" >Shipped</option>
+              <option value="Delivered" >Delivered</option>
+              <option value="Cancelled" >Cancelled</option>
+            </select>
+            <button 
+              onClick={()=>handleUpdateStatus(currentOrder.id, orderStatus)}
+              className='w-auto px-4 bg-blue-600 text-white font-semibold py-2 rounded-lg hover:bg-blue-700 transition'>
+              Update Status
+            </button>
+            <button 
+              onClick={()=> setUpdateStatus(false)}
+              className='w-auto px-4 bg-red-600 text-white font-semibold py-2 rounded-lg hover:bg-red-700 transition'>
+              Cancel
+            </button>
+          </div>
+        </div>
+      ):null}
+
+      <div className={`my-20 mt-12 md:ms-64 ms-24 ${
+            updateStatus ? 'filter blur-md' : ''
+        }`}>
         <h1 style={{color:'#052560'}} className='text-center font-bold text-2xl'>Recent Orders</h1>
         <div className='w-full p-10  flex gap-4 justify-center flex-wrap'>
             {orderDetails.map((order, index)=>(
@@ -77,6 +188,32 @@ const OrdersAdmin = () => {
                   <span className='font-semibold'>Transactiom ID : </span>
                   <span>{order.transactionId}</span>
                 </p>
+
+                {/* Order Status */}
+                <div className='flex justify-center gap-2 text-white pt-3'>
+                  <p className={`w-3/7 rounded-lg font-semibold px-5 pt-2 ${
+                      order.orderstatus === 'Pending'
+                        ? 'bg-gray-500'
+                        : order.orderstatus === 'Shipped'
+                        ? 'bg-blue-500'
+                        : order.orderstatus === 'Delivered'
+                        ? 'bg-green-500'
+                        : order.orderstatus === 'Cancelled'
+                        ? 'bg-red-500'
+                        : ''
+                        } text-white`}>
+                    Order {order.orderstatus}
+                  </p>
+
+                  <div className='w-3/7 flex justify-center'>
+                    <button 
+                      onClick={()=>showUpdateModal(order.id)}
+                      className='w-full px-5  bg-blue-600 text-white font-semibold py-2 rounded-lg hover:bg-blue-700 transition'>
+                      Update Status
+                    </button>
+                  </div>
+                </div>
+
 
               </div>
             ))}
